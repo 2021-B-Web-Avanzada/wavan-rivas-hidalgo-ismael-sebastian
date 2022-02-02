69e631da1575e20ebdebb4b82167d006af9724a6
Ismael <ismael.rivas@epn.edu.ec>
2022-02-02 08:56:02 -0500

    Clase 33 - Websockets 2

--- 04-websockets/websockets/src/app.module.ts
+++ 04-websockets/websockets/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from "@nestjs/common";
+import {EventosModule} from "./eventos/eventos.module";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [EventosModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule{
+}
